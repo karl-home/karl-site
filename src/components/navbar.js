79d0bef0e845db4713daeb604226dc4e0ad3b8ac
Henry Weng <henryweng2003@gmail.com>
2021-08-10 10:31:51 -0700

    Changed site name

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar() {
   return (
     <div id="top" className="flex items-center justify-left h-auto border-b border-white border-opacity-40 bg-cardinal z-40">
-      <h1 className="text-2xl font-bold py-3 pl-4">Karl Website</h1>
+      <a href="/"><h1 className="text-2xl font-bold py-3 pl-4">Karl Website</h1></a>
       <div className="absolute right-0 pr-4 py-3">
         <div className="flex">
           <a href="/">
@@ -11,14 +11,14 @@ export default function Navbar() {
               Home
             </p>
           </a>
-          <a href="/people">
-            <p className="nav-link">
-              People
-            </p>
-          </a>
           <a href="/modules">
             <p className="nav-link">
               Modules
+            </p>
+          </a>
+          <a href="/people">
+            <p className="nav-link">
+              People
             </p>
           </a>
         </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -9,6 +9,7 @@ export default function Home() {
     <div>
       <HomeHero />
       <div className="center text-container">
+        <h2 className="pb-2 text-center">About</h2>
         <p>
           Karl is a privacy-preserving framework that IoT devices can use to execute as much functionality as possible on a home cloud that runs on user-owned hardware. Rather than relying on third-party cloud services and local device state, devices (sensors) in the home cloud express their functionality in terms of small, ephemeral units of computation (similar to serverless) called modules. When functionality must be offloaded to the cloud, users define pipeline policies that justify under which conditions data can be exfiltrated. For details, see our preprint&nbsp;
           <a className="link" target="_blank" rel="noopener noreferrer" href="http://www.ginayuan.com/preprints/preprint-karl.pdf">
